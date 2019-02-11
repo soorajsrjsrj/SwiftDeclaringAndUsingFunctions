5ef720c6617f62d9830393c5d4872a1de94c96d9
moxDroid Labs Inc <pritam.world@gmail.com>
2019-02-11 17:58:39 -0500

    Initial Commit

---
+++ SwiftDeclaringAndUsingFunctions/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftDeclaringAndUsingFunctions
+//
+//  Created by moxDroid on 2019-02-11.
+//  Copyright © 2019 moxDroid. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
